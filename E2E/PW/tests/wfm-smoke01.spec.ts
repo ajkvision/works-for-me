07d847b572c3bd3f4ed702b618398a3b11b22a93
Andrzej Kwiecien <andrzej.kwiecien@ajkvision.eu>
2024-03-23 21:07:45 +0100

    #2 Added E2E  Tests in Playwright - init version, first test, learinig/checking Playwright

---
+++ E2E/PW/tests/wfm-smoke01.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('http://localhost:5265/Home/TestPlans/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Test plans/);
+});
+
+test('home link', async ({ page }) => {
+  await page.goto('http://localhost:5265/Home/TestPlans/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Home' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Welcome' })).toBeVisible();
+});
