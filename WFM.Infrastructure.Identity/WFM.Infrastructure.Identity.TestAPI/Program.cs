34236afee2f0908eee2bc2f78b48a7f8d8f53105
Andrzej Kwiecien <andrzej.kwiecien@ajkvision.eu>
2024-04-18 19:28:21 +0200

    #2 Implementation of OIDC auth/duende Identity Server. Test projects and test configuration.

--- WFM.Infrastructure.Identity/WFM.Infrastructure.Identity.TestAPI/Program.cs
+++ WFM.Infrastructure.Identity/WFM.Infrastructure.Identity.TestAPI/Program.cs
@@ -1,9 +1,19 @@
+using System.Security.Claims;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication()
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://localhost:5001";
+        options.TokenValidationParameters.ValidateAudience = false;
+    });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -15,6 +25,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("identity", (ClaimsPrincipal user) => user.Claims.Select(c => new { c.Type, c.Value }))
+    .RequireAuthorization();
+
 
 var summaries = new[]
 {
---
+++ WFM.Infrastructure.Identity/WFM.Infrastructure.Identity.TestClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using IdentityModel.Client;
+// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+
+// discover endpoints from metadata
+var client = new HttpClient();
+var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+if (disco.IsError)
+{
+    Console.WriteLine(disco.Error);
+}
+else
+{
+    Console.WriteLine(disco.ToString());
+}
+
+
+Console.WriteLine("End");
+Console.ReadLine();
