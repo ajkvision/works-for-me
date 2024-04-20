af8bd78288b45db44249fc28a276913a8671cbe1
Andrzej Kwiecien <andrzej.kwiecien@ajkvision.eu>
2024-04-20 19:54:04 +0200

    #2 Implementation of OIDC auth/duende Identity Server. Test projects and test configuration. Added test client, connecting to test api.

--- WFM.Infrastructure.Identity/WFM.Infrastructure.Identity.TestClient/Program.cs
+++ WFM.Infrastructure.Identity/WFM.Infrastructure.Identity.TestClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using IdentityModel.Client;
+﻿using System.Text.Json;
+using IdentityModel.Client;
 // See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
@@ -39,6 +40,22 @@ else
 }
 
 Console.WriteLine(tokenResponse.AccessToken);
+
+// call api
+var apiClient = new HttpClient();
+apiClient.SetBearerToken(tokenResponse.AccessToken!); // AccessToken is always non-null when IsError is false
+
+var response = await apiClient.GetAsync("https://localhost:7187/identity");
+if (!response.IsSuccessStatusCode)
+{
+    Console.WriteLine(response.StatusCode);
+}
+else
+{
+    var doc = JsonDocument.Parse(await response.Content.ReadAsStringAsync()).RootElement;
+    Console.WriteLine(JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true }));
+}
+
 
 Console.WriteLine("End");
 Console.ReadLine();
