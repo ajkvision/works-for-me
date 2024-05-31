d3ca687937627af39b2b7755509ef08df611be0f
ajkvision <70810585+ajkvision@users.noreply.github.com>
2024-05-31 22:13:31 +0200

    Update jira_api_tests.py

--- snippets/jira_api_tests.py
+++ snippets/jira_api_tests.py
@@ -1,7 +1,14 @@
 from atlassian import Jira
+from atlassian import Confluence
 
 jira_instance = "https://my.atlassian.net"
 
 jira = Jira(jira_instance, username="my@email.com", password="API-TOKEN-HERE", cloud=True)
+
+confluence = Confluence(
+    url='https://your-domain.atlassian.net',
+    username=atlassian_username,
+    password=atlassian_api_token,
+    cloud=True)
 
 print(jira.api_version)
