1649a6b66163c5dd9575af3e66a63097e9267982
ajkvision <70810585+ajkvision@users.noreply.github.com>
2024-06-01 23:19:09 +0200

    Update jira_api_tests.py - access jira issues tests

--- snippets/jira_api_tests.py
+++ snippets/jira_api_tests.py
@@ -5,6 +5,9 @@ jira_instance = "https://my.atlassian.net"
 
 jira = Jira(jira_instance, username="my@email.com", password="API-TOKEN-HERE", cloud=True)
 
+test_jira_key = 'TST-1'
+jira_issue = jira.issue(key)
+
 confluence = Confluence(
     url='https://your-domain.atlassian.net',
     username=atlassian_username,
