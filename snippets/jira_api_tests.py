0bf3856be41c5e851ec4f3639e81a92850155be2
Andrzej Kwiecien <andrzej.kwiecien@ajkvision.eu>
2024-06-02 22:26:57 +0200

    #2 small amends - JIRA API exploration

--- snippets/jira_api_tests.py
+++ snippets/jira_api_tests.py
@@ -6,7 +6,7 @@ jira_instance = "https://my.atlassian.net"
 jira = Jira(jira_instance, username="my@email.com", password="API-TOKEN-HERE", cloud=True)
 
 test_jira_key = 'TST-1'
-jira_issue = jira.issue(key)
+jira_issue = jira.issue(test_jira_key)
 
 confluence = Confluence(
     url='https://your-domain.atlassian.net',
