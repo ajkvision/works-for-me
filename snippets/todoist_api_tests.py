161809e2dfe8a23f7552ee0583d0f1456ad0d887
ajkvision <70810585+ajkvision@users.noreply.github.com>
2024-06-08 22:45:35 +0200

    Update todoist_api_tests.py

--- snippets/todoist_api_tests.py
+++ snippets/todoist_api_tests.py
@@ -2,6 +2,14 @@ from todoist_api_python.api import TodoistAPI
 
 api = TodoistAPI("")
 
+#create prject - for testc case execution 
+
+try:
+    project = api.add_project(name="Test Execution - Smoke test 01")
+    print(project)
+except Exception as error:
+    print(error)
+
 try:
     projects = api.get_projects()
     print(projects)
